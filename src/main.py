161ab142e150daec92fb27624361c38e1b8e46ff
jackyang <467287824@qq.com>
2017-05-24 22:52:54 +0800

    update

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from PIL import Image
 import serial
+import time
+women n
 
 def read_image(path):
     '''
@@ -12,16 +14,16 @@ def read_image(path):
     pix = im.load()
     width = im.size[0]
     height = im.size[1]
-    print("width=%d,height=%d\n"%(width,height))
+    print("width=%d,height=%d\n" % (width, height))
     for x in range(height):
         for y in range(width):
             r, g, b = pix[y, x]
-            pix_arr.append(((r&7)<<5)+((g&7)<<2)+((b&3)))
+            pix_arr.append(((r & 7) << 5) + ((g & 7) << 2) + ((b & 3)))
     return pix_arr
 
 
-ser = serial.Serial('/dev/ttyAMA0',115200)
-
+ser = serial.Serial('COM3', 115200)
+if not ser.isOpen():
+    exit(-1)
 
 pix_arr = read_image('../pic/black.jpg')
-
